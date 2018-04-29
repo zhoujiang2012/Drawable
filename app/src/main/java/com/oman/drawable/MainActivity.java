3c51c481db40df5973e84c0930f7c3ba484177ca
zhoujiang <zhoujiang2012@163.com>
2018-04-29 13:44:29 +0800

    Initial commit

---
+++ app/src/main/java/com/oman/drawable/MainActivity.java
@@ -0,0 +1,22 @@
+package com.oman.drawable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+/**
+ * @author 15936
+ * 1
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "aaa";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
